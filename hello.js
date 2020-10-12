5886a2f8379e2b5ae9f1cd31e5fb2449576458a3
Grace Tzay <grace.tzay@globant.com>
2020-10-12 12:05:50 -0700

    hello react js

--- hello.js
+++ hello.js
@@ -1,4 +1,14 @@
-ReactDOM.render(
-  <h1>Hello, React</h1>,
-  document.getElementById('root')
-);
+import React from 'react';
+
+const HelloWorld = () => {
+  
+  function sayHello() {
+    alert('Hello, World!');
+  }
+  
+  return (
+    <button onClick={sayHello}>Click me!</button>
+  );
+};
+
+export default HelloWorld;
