ce964c34077bb9e42165e287ba4f5f16b879101f
Grace <gracesite38@gmail.com>
2020-10-13 20:39:07 -0700

    form stuff

--- src/index.js
+++ src/index.js
@@ -4,12 +4,40 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class MyForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      age: null,
+      errormessage: ''
+    };
+  }//constructor
+  myChangeHandler = (event) => {
+    let nam = event.target.name;
+    let age = event.target.age;
+    let err = '';
+    if (nam === "age"){
+      if (!Number(val)){
+        err = <strong> Age must be digits </strong>;
+      }
+    }
+    this.setState({errormessage: err});
+    this.setState({[nam]: val});
+  }
+  render() {
+    return(
+      <form>
+        <p> name:</p> 
+        <input type='text' name='username' onChange={this.myChangeHandler} />
+        <p> age: </p>
+        <input type='text' name='age' onChange={this.myChangeHandler} />
+        {this.state.errormessage}
+      </form>
+    );
+  }
+}
+ReactDOM.render(<MyForm/>, document.getElementById('myform6'))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
